32188d3e389232b07cdb6cb5475b672ab7ef44fa
Aakanksha <aakanksha.kotha@thoughtworks.com>
2023-03-09 11:37:21 +0530

    Testing lint-staged

--- src/components/Home/categories.jsx
+++ src/components/Home/categories.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-key */
+/* eslint-disable react/prop-types */
 /* eslint-disable react/react-in-jsx-scope */
 import './homepage.css';
 const Category = ({ categories }) => {
